0cde4ad4bcf673368130b45e55b73c5d6f40b394
Danny Ruiz <ynnadrules@gmail.com>
2019-12-28 16:38:12 -0600

    fix(ruby): fix command check

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,5 +1,5 @@
 # TODO: check if system has other ruby than system ruby
-if (( $+commands[rbenv] ))
+if (( $+command[rbenv] ))
 then
   echo "Installing latest stable Ruby"
   rbenv install $(rbenv install -l | grep -v - | tail -1 | tr -d '[[:space:]]')
