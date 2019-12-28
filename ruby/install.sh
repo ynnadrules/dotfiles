328b6987f5c6acfb8819d5d61e85ce388e568a5b
Danny Ruiz <ynnadrules@gmail.com>
2019-12-28 16:52:20 -0600

    fix(rbenv/git): fix syntax for rbenv check; dont force ssh with github

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,5 +1,5 @@
 # TODO: check if system has other ruby than system ruby
-if (( $+command[rbenv] ))
+if command -v rbenv >/dev/null 2>&1
 then
   echo "Installing latest stable Ruby"
   rbenv install $(rbenv install -l | grep -v - | tail -1 | tr -d '[[:space:]]')
